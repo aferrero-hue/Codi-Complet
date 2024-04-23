a44bc8b0c890232f432fb3c614944f5de270a06a
aferrero-hue <66164925+aferrero-hue@users.noreply.github.com>
2024-04-23 17:23:25 +0200

    Resolving conflicts

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,3 +1,19 @@
-function test(){
+/*const { MongoClient } = require('mongodb')
+const { connect } = require('mongoose')
 
-}
+let dbConnection
+let uri = 'mongodb+srv://aferrerodaw:VFRHUPWzcCvclNax@cluster0.nxj9ol6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+module.exports = { 
+    connectToDb: (cb) => {
+        MongoClient.connect(uri)
+        .then((client) => {
+            dbConnection = client.db()
+            return cb()
+        }).catch(err => {
+            console.log(err)
+            return cb(err)
+        })
+    },
+    getDb: () => dbConnection
+}*/
