2605d61232eb42d4b8abc80aa92576c66f0a8550
aferrero-hue <66164925+aferrero-hue@users.noreply.github.com>
2024-04-23 19:11:35 +0200

    NOSE

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,19 +1,27 @@
-/*const { MongoClient } = require('mongodb')
-const { connect } = require('mongoose')
+const MongoClient = require('mongodb').MongoClient;
 
-let dbConnection
-let uri = 'mongodb+srv://aferrerodaw:VFRHUPWzcCvclNax@cluster0.nxj9ol6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+async function connectToMongoDB(connectionString) {
+    try {
+        const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        console.log("Connected to MongoDB successfully!");
+        return client;
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+}
 
-module.exports = { 
-    connectToDb: (cb) => {
-        MongoClient.connect(uri)
-        .then((client) => {
-            dbConnection = client.db()
-            return cb()
-        }).catch(err => {
-            console.log(err)
-            return cb(err)
-        })
-    },
-    getDb: () => dbConnection
-}*/
+// Ejemplo de uso:
+const connectionString = "mongodb+srv://aferrerodaw:VFRHUPWzcCvclNax@cluster0.nxj9ol6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+connectToMongoDB(connectionString)
+    .then(client => {
+        // Aquí puedes realizar operaciones en la base de datos utilizando 'client'
+        // Por ejemplo:
+        // const db = client.db("nombre_de_tu_base_de_datos");
+        // const collection = db.collection("nombre_de_tu_coleccion");
+        // Ahora puedes realizar operaciones en la colección
+    })
+    .catch(error => {
+        // Manejar el error si la conexión falla
+    });
